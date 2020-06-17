d73e4e68984a03fa7b5a4308d0820df257cb13bd
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-06-17 22:38:52 +0600

    Recursion

---
+++ Recursion.java
@@ -0,0 +1,24 @@
+/*
+    Author : Arnob Mahmud 
+    
+    Mail : arnob.tech.me@gmail.com
+*/
+
+public class Recursion {
+
+  public static void main(String[] args) {
+
+    int result = sum(10);
+    System.out.println(result);
+  }
+
+  public static int sum(int k) {
+
+    if(k > 0) {
+      return k + sum(k - 1);
+    } else {
+
+      return 0;
+    }
+  }
+}
