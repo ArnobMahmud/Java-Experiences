0a8a76e138647be897e35cf0e05e28d0baa955cc
ArnobMahmud <arnob.tech.me@gmail.com>
2020-09-25 11:39:51 +0600

    Input by User

---
+++ Basics/ScannerInput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class goodMorning {
+    private static Scanner input;
+
+    public static void main(String[] args) {
+
+        String person;
+        
+        input = new Scanner(System.in);
+        person = input.nextLine();
+        morningWish(person);
+    }
+
+    public static void morningWish(String girl) {
+        System.out.println("Good Morning " + girl);
+    }
+}
