db92f2b3fa98859188f5163911381c9c52757ac8
ArnobMahmud <arnob.tech.me@gmail.com>
2020-09-28 12:04:04 +0600

    math files added

---
+++ Maths/serialSum.java
@@ -0,0 +1,29 @@
+/* 
+   Author : Arnob Mahmud
+
+   Mail : arnob.tech.me @ gmail.com  
+*/
+
+package Maths;
+import java.util.Scanner;
+
+public class serialSum {
+    private static Scanner input;
+    public static void main(final String[] args) {
+        int m, n;
+        int sum = 0;
+
+        input = new Scanner(System.in);
+
+        System.out.println("Enter a starting number : ");
+        m = input.nextInt();
+
+        System.out.println("Enter a ending number : ");
+        n = input.nextInt();
+
+        for (int i = m; i < n; i++) {
+            sum = sum + i;
+        }
+        System.out.println("Sum is : " + sum);
+    }
+}
