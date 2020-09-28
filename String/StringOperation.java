fce5efcc563544a41b7b80b3e01123b79b2e2c6e
ArnobMahmud <arnob.tech.me@gmail.com>
2020-09-28 19:29:55 +0600

    String Operation

---
+++ String/StringOperation.java
@@ -0,0 +1,14 @@
+package String;
+
+public class StringOperation {
+    public static void main(String[] args) {
+        String myFirstNmae = "Arnob";
+        String myLastNmae = " Mahmud";
+
+        String myFullName = myFirstNmae + myLastNmae;
+        System.out.println(myFullName);
+
+        System.out.println(myFullName.toUpperCase());
+        System.out.println(myFullName.toLowerCase());
+    }
+}
