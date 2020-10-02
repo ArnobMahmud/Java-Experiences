74c497a7a6051ca8c20ab9ca4ac95d3f114530b7
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-02 19:54:47 +0600

    OOP Method

---
+++ OOP/Method/MainClass.java
@@ -0,0 +1,8 @@
+package OOP.Method;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MyClass object = new MyClass();
+        object.myFun(55, 77);
+    }
+}
---
+++ OOP/Method/MyClass.java
@@ -0,0 +1,12 @@
+package OOP.Method;
+
+public class MyClass {
+
+    public void myFun(int x, int y) {
+        int sum;
+
+        sum = x + y;
+        System.out.println(sum);
+
+    }
+}
