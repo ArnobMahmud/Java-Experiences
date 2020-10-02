b4196e562819040558ca21d4619ade3185703d88
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-02 19:35:59 +0600

    Java OOP Concept

---
+++ OOP/ClassIntroductory/ClassIntroductory.java
@@ -0,0 +1,11 @@
+package OOP.ClassIntroductory;
+
+public class ClassIntroductory{
+    
+    public static void main(String[] args) {
+        
+        MyCreatedClass objectVariable = new MyCreatedClass();
+        objectVariable.myFun();
+
+    }
+}
---
+++ OOP/ClassIntroductory/MyCreatedClass.java
@@ -0,0 +1,8 @@
+package OOP.ClassIntroductory;
+
+public class MyCreatedClass {
+    
+    public void myFun() {
+        System.out.println("Hello OOP");
+    }
+}
