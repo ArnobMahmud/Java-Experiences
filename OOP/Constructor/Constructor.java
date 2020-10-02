1b99545c9ad01897a358c0bf0044304ebedf1dde
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-02 22:02:36 +0600

    Constructor

---
+++ OOP/Constructor/Constructor.java
@@ -0,0 +1,9 @@
+package OOP.Constructor;
+
+public class Constructor {
+    public static void main(String[] args) {
+        
+        MyClass object = new MyClass(10);
+        object.sum(23, 45);
+    }
+}
---
+++ OOP/Constructor/MyClass.java
@@ -0,0 +1,16 @@
+package OOP.Constructor;
+
+public class MyClass {
+
+    MyClass(int a){
+
+        int x = a;
+        int y = 12;
+        System.out.println( "Sum is : " + x * y);
+    }
+
+    public void sum(int m, int n) {
+        int sum = m * n;
+        System.out.println("Sum is : " +sum);
+    }
+}
