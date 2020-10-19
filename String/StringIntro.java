a8eb30cbf97bacd05abc6046d3ab9735c8bcbd95
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-19 18:58:01 +0600

    String in JAVA

---
+++ String/StringIntro.java
@@ -0,0 +1,17 @@
+/*
+    Author : Arnob Mahmud
+
+    Mail : arnob.tech.me@gmail.com
+*/
+
+package String;
+
+public class StringIntro {
+    public static void main(String[] args) {
+
+        String s1 = "Arnob ";
+        String s2 = new String("Mahmud");
+
+        System.out.println(s1 + s2);
+    }
+}
