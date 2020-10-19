317dd6753767fa8328e69c37efd7b6d52073019a
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-19 19:24:06 +0600

    String operation

---
+++ String/StringBuilderWork.java
@@ -0,0 +1,21 @@
+/*
+    Author : Arnob Mahmud
+
+    Mail : arnob.tech.me@gmail.com
+*/
+
+package String;
+
+public class StringBuilderWork {
+    public static void main(String[] args) {
+
+        StringBuilder str = new StringBuilder("Arnob");       // changable string 
+
+        str.append(" Mahmud");      // joins two string
+        System.out.println(str);
+
+        System.out.println(str.reverse());
+
+        System.out.println(str.delete(5, 9));  // M, " ", b, o will be deleted
+    }
+}
