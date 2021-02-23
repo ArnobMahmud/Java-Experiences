d2d057c4f5aa3d7e9b896349e4ebeaef6c605975
ArnobMahmud <arnob.tech.me@gmail.com>
2021-02-23 18:43:58 +0600

    w3Resources inti

---
+++ w3Resources/comments.java
@@ -0,0 +1,18 @@
+package w3Resources;
+
+public class comments {
+    public static void main(String[] args) {
+
+        // this is a comment
+        System.out.println("Hello world!");
+    }
+}
+
+
+// Comments can be used to explain Java code, and to make it more readable. It can also be used to prevent execution when testing alternative code.
+
+// Single-line comments start with two forward slashes (//).
+
+// Any text between // and the end of the line is ignored by Java (will not be executed).
+
+// This example uses a single-line comment before a line of code:
