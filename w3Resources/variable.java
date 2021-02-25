015f63f8a609dea8dfd69c4be7776dff9d95e659
ArnobMahmud <arnob.tech.me@gmail.com>
2021-02-25 21:11:34 +0600

    new files added

---
+++ w3Resources/variable.java
@@ -0,0 +1,28 @@
+package w3Resources;
+
+public class variable {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println("Hello " + name);
+    }
+}
+
+
+// Variables are containers for
+// storing data
+// values.
+
+//         In Java, there
+// are different
+// types of variables,for example:
+
+// String-
+// stores text, such as"Hello".
+// String values
+// are surrounded by
+// double quotes int-
+
+// stores integers (whole numbers), without decimals, such as 123 or -123
+// float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+// char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+// boolean - stores values with two states: true or false
