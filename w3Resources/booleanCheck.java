0cb1c61723d92433f43d825cfefdb63e304121b8
ArnobMahmud <arnob.tech.me@gmail.com>
2021-02-26 20:51:28 +0600

    folder update

---
+++ w3Resources/booleanCheck.java
@@ -0,0 +1,10 @@
+package w3Resources;
+
+public class booleanCheck {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun); // Outputs true
+        System.out.println(isFishTasty); // Outputs false
+    }
+}
