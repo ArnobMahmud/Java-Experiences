11a1ae91cf273bfe7728cded09c109dddf6b4c62
ArnobMahmud <arnob.tech.me@gmail.com>
2021-02-27 20:23:43 +0600

    arrays

---
+++ w3Resources/arrayLoops.java
@@ -0,0 +1,16 @@
+package w3Resources;
+
+public class arrayLoops {
+    public static void main(String[] args) {
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
+
+
+// Loop Through an Array
+// You can loop through the array elements with the for loop, and use the length property to specify how many times the loop should run.
+
+// The following example outputs all elements in the cars array:
---
+++ w3Resources/arrays.java
@@ -0,0 +1,29 @@
+package w3Resources;
+
+public class arrays {
+    public static void main(String[] args) {
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        System.out.println(cars[0]);
+        // Outputs Volvo
+
+        
+    // To find out how many elements an array has, use the length property:
+        System.out.println(cars.length);
+        // Outputs 4
+    }
+}
+
+
+//java Arrays
+// Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+// To declare an array, define the variable type with square brackets:
+
+// String[] cars;
+// We have now declared a variable that holds an array of strings. To insert values to it, we can use an array literal - place the values in a comma-separated list, inside curly braces:
+
+// String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+// To create an array of integers, you could write:
+
+// int[] myNum = {10, 20, 30, 40};
