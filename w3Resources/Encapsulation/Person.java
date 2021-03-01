a9edb9dbe6361fab0d23d773b0894f3e98e5a588
ArnobMahmud <arnob.tech.me@gmail.com>
2021-03-01 20:50:30 +0600

    encapsulation

---
+++ w3Resources/Encapsulation/Person.java
@@ -0,0 +1,15 @@
+package w3Resources.Encapsulation;
+
+public class Person {
+    private String name; // private = restricted access
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
