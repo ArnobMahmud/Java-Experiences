f88dc3f1964d03c9d259c9866d4bef6109afc802
ArnobMahmud <arnob.tech.me@gmail.com>
2021-03-01 20:27:48 +0600

    java classes and objects

---
+++ w3Resources/classMethod.java
@@ -0,0 +1,12 @@
+package w3Resources;
+
+public class classMethod {
+    
+    static void myMethod() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
---
+++ w3Resources/objectsIntro.java
@@ -0,0 +1,9 @@
+package w3Resources;
+
+public class objectsIntro {
+    int x = 7;
+    public static void main(String[] args) {
+        objectsIntro obj = new objectsIntro();
+        System.out.println(obj.x);
+    }
+}
