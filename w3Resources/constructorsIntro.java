ef4b037a89c9dba055e2ed153db99160690a814b
ArnobMahmud <arnob.tech.me@gmail.com>
2021-03-01 20:37:13 +0600

    OOP secs

---
+++ w3Resources/constructorsIntro.java
@@ -0,0 +1,17 @@
+package w3Resources;
+
+public class constructorsIntro {
+    int x; // Create a class attribute
+
+    // Create a class constructor for the constructorsIntro class
+    public constructorsIntro() {
+        x = 7;  // Set the initial value for the class attribute x
+    }
+
+    public static void main(String[] args) {
+        constructorsIntro myObj = new constructorsIntro(); // Create an object of class constructorsIntro (This will call the constructor)
+        System.out.println(myObj.x);  // Print the value of x
+    }
+}
+
+// Outputs 7
