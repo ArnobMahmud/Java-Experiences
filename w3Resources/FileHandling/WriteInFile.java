e0915788832a7b0f25fada8f87295b832457e160
ArnobMahmud <arnob.tech.me@gmail.com>
2021-03-04 19:09:36 +0600

    File Create

---
+++ w3Resources/FileHandling/WriteInFile.java
@@ -0,0 +1,18 @@
+package w3Resources.FileHandling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteInFile {
+    public static void main(String[] args) {
+        try {
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
