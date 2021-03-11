774299066c1ef4d7fcbe3084ea4401cdad4ce94e
ArnobMahmud <arnob.tech.me@gmail.com>
2021-03-11 20:33:44 +0600

    Wrapper

---
+++ w3Resources/ClassFunctionalities/WrapperClasses.java
@@ -0,0 +1,17 @@
+package w3Resources.ClassFunctionalities;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+
+        Integer myInt = 5;
+        Double myDouble = 5.99;
+        Character myChar = 'A';
+        System.out.println(myInt.intValue());
+        System.out.println(myDouble.doubleValue());
+        System.out.println(myChar.charValue());
+    }
+}
+
+// ArrayList<int>myNumbers=new ArrayList<int>(); // Invalid
+
+// ArrayList<Integer> myNumbers = new ArrayList<Integer>();// Valid
